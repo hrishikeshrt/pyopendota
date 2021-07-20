d36d6055e626a190dd77e5c960cdf5e786f29081
Hrishikesh Terdalkar <hrishirt@cse.iitk.ac.in>
2021-07-20 15:14:00 +0530

    Bump version: 0.1.0 → 0.1.1

--- opendota/__init__.py
+++ opendota/__init__.py
@@ -4,6 +4,6 @@ from opendota.opendota import *
 
 __author__ = """Hrishikesh Terdalkar"""
 __email__ = 'hrishikeshrt@linuxmail.org'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 __all__ = [OpenDota, FREQ_LOW, FREQ_MEDIUM, FREQ_HIGH]
