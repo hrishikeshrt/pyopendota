13a74fc3d16bd20ae780a6119d47271f078884df
Hrishikesh Terdalkar <hrishirt@cse.iitk.ac.in>
2022-08-22 16:11:57 +0530

    Bump version: 0.2.7 → 0.2.8

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/hrishikeshrt/pyopendota',
-    version='0.2.7',
+    version='0.2.8',
     zip_safe=False,
 )
